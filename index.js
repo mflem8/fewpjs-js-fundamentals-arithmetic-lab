3c00ad2f67cc744ed1ffe8e8e40fe83e4e86ad4d
mflem8 <matthewfleming8@gmail.com>
2021-07-08 16:01:25 -0400

    commit tree 2585b6809d9935c4305f67aa591ab1c74f99ce8e

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const num3 = 44;
+const num4 = 10;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 9, 15, 20, 19, 2, 13);
